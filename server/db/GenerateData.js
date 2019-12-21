1b93f4a49b4e0e09cc025573c136eb0c615c0287
Micah <micahdshackelford@gmail.com>
2019-12-20 20:17:46 -0500

    Fix createcsv

--- server/db/GenerateData.js
+++ server/db/GenerateData.js
@@ -1,5 +1,4 @@
 const fs          = require('fs'),
-      stringify   = require('csv-stringify'),
       path        = require('path'),
       faker       = require('faker'),
       images      = require('./images');
@@ -9,20 +8,16 @@ const CreateCSV = () => {
     const writer = fs.createWriteStream(path.join(__dirname,`/seedData/csv-chunk-${a}.csv`));
     writer.write('id,images,name\n', 'utf8');
     for(var b = 0; b < 2500000; b++) {
-      if(b < 1000000) {
-        const numPhotos = Math.floor(Math.random() * (20 - 10)) + 10;
-        let photos = [];
-        for(i = 0; i < numPhotos; i++) {
-          const photo = images[Math.floor(Math.random() * (1008 - 0)) + 0]
-          photos.push(photo);
-        }
-        console.log(`[Seed] Generating Entry ([${a+1}/4] ${b+1}/2500000)`);
-        writer.write(`${(a * 2500000) + (b+1)},"[${photos}]",${faker.lorem.word()}\n`);
-      }else {
-        writer.end();
+      const numPhotos = Math.floor(Math.random() * (20 - 10)) + 10;
+      let photos = [];
+      for(i = 0; i < numPhotos; i++) {
+        const photo = images[Math.floor(Math.random() * (1008 - 0)) + 0]
+        photos.push(photo);
       }
+      console.log(`[Seed] Generating Entry ([${a+1}/4] ${b+1}/2500000)`);
+      writer.write(`${(a * 2500000) + (b+1)},"[${photos}]",${faker.lorem.word()}\n`);
     }
   }
 }
 
-module.exports = CreateCSV();
+CreateCSV();
