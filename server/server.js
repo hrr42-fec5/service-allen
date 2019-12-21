429c1221a536ec15f97a4f31e6e06545ee249910
Micah <micahdshackelford@gmail.com>
2019-12-21 18:34:12 -0500

    Complete refactor of api to support ScyllaDB

--- server/server.js
+++ server/server.js
@@ -1,40 +1,79 @@
-const express = require('express');
+const express = require('express'),
+      client  = require('./db/database');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 app.post('/api/images/',(req,res,next) => { // Create
-  // Grab information from request
-  // Format
-  // Send off to db
-  // Return response
+  const query = `INSERT INTO zagat.restaurants (id,images,name) VALUES (${req.body.id},${req.body.images},${req.body.name})`;
+
+  client.execute(query)
+    .then(() => {
+      res.send({
+        ok: true,
+        message: "Post success"
+      })
+    }).catch(() => {
+      res.send({
+        ok: false,
+        message: "Post error"
+      })
+    })
 });
 
 app.get('/api/images/:restaurantId',(req,res,next) => { // Read
-  // Grab the images from the database
-  // Return to client
+  const id = parseInt(req.params.restaurantId);
+
+  const query = `SELECT * FROM restaurants WHERE id = ${id}`;
+  client.execute(query)
+    .then((result) => {
+      res.send(result.rows[0]);
+    });
 });
 
 app.patch('/api/images/:restaurantId',(req,res,next) => { // Update
-  // Find images from database
-  // If exists
-    // update
-  // Else
-    // Return error
+  // req.body.updates should be an array of changes (ex: ["name=hello","images=['googe.com']"])
+  // const query = `UPDATE ${...req.body.updates} FROM restaurants WHERE id = ${req.body.id}`;
 
-  //return success
+  let updates = "";
+  for(var i = 0; i < req.body.updates.length; i++) {
+    updates += req.body.updates[i] + " ";
+  }
+
+  const query = `UPDATE ${updates} FROM restaurants WHERE id = ${req.body.id}`;
+  client.execute(query)
+    .then(() => {
+      res.send({
+        ok: true,
+        message: "Updated"
+      })
+    }).catch(() => {
+      res.send({
+        ok: false,
+        message: "Update error"
+      })
+    })
 });
 
 app.delete('/api/images/:restaurantId',(req,res,next) => { // Delete
-  // Find images from database
-  // If exists
-    // delete
-  // Else
-    // Return error
-
-  //return success
+  const query = `DELETE FROM zagat.restaurants WHERE id = ${req.body.id}`;
+  client.execute(query)
+    .then(() => {
+      res.send({
+        ok: true,
+        message: "Deleted"
+      })
+    }).catch(() => {
+      res.send({
+        ok: false,
+        message: "Delete error"
+      })
+    })
 });
 
-app.listen((err) => {
+app.listen(PORT, (err) => {
   console.log(`[Server] Running on port ${PORT}`);
 })
